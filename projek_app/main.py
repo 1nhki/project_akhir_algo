7334a5cd670e96d5441caac83e7a4e180665b7bf
inhki <inhki@outlook.co.id>
2024-09-28 22:54:34 +0700

    add a bunch of shit

--- projek_app/main.py
+++ projek_app/main.py
@@ -2,40 +2,75 @@ from PyQt5.QtCore import (Qt, QSize)
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,QLabel, QVBoxLayout, QHBoxLayout, QRadioButton,
                              QGroupBox, QButtonGroup, QLineEdit, QCheckBox, QListWidget)
 from PyQt5.QtGui import (QPixmap, QIcon, QImage, QPalette, QBrush) #ini belum ada
-from random import randint, shuffle, seed
+from random import randint,choice, seed, random
 from time import sleep, time_ns
+import json
 
 app = QApplication([])
 class MainWindow(QWidget):
-    def __init__(self, title, layout):
+    def __init__(self, title, layout, size):
         super().__init__()
-        self.setWindowTitle(title)
-        self.resize(450, 500)
+        self.setWindowTitle(title)                  
+        self.resize(size[0], size[1])
         self.setLayout(layout)
         self.setWindowIcon(QIcon("projek_app/asset/home_14782571.png"))
-        self.bg_img = QImage("projek_app/asset/placard-templates-set-with-abstract-shapes-80s-memphis-geometric-style-flat-line-design-elements-ret/391481422432.jpg")
-                                        
+        self.bg_img = QImage("projek_app/asset/black-brick-wall-textured-background.jpg")
+        alamak= QPalette()
+        alamak.setBrush(QPalette.Window, QBrush(self.bg_img))
+        self.setPalette(alamak)
+class Button(QPushButton):
+    def __init__(self,size, string, font = 50):
+        super().__init__()
+        self.setText(string)        
+        self.setFixedSize(size[0], size[1])
+        css = f"""      border : 5px solid black;  
+                        border-radius : 20px;
+                        background-color : white;
+                        font : {font}px arial;
+                        color : black;
+            """
+        self.setStyleSheet(css)
+
+  
+
 
 """listwidget"""
+"""iwak"""
+label_iwak_pool = QListWidget()
+button_iwak = []
+button_iwak.append(QPushButton("shoot"))
+button_iwak.append(QPushButton("inventory"))
+
 """SHOOT"""
 label_shooting_range = QListWidget()
+label_shooting_range.setStyleSheet("background-image : url(projek_app/asset/solid-painted-concrete-wall-textured-backdrop.jpg)")
 label_shooting_range.addItem("u and ur oppoent have 3 hp")
 label_shooting_range.setFixedSize(300, 350)
 button_shooting = []
-button_shooting.append(QPushButton("shoot"))
-button_shooting.append(QPushButton("inventory"))
-
+button_shooting.append(Button((200, 100),"shoot", 40))
+button_shooting.append(Button((200, 100),"inventory", 40))
 
 """HIGH_LOW"""
 label_HIGH_LOW = QListWidget()
+label_HIGH_LOW.setStyleSheet("background-image : url(projek_app/asset/solid-painted-concrete-wall-textured-backdrop.jpg)")
 label_HIGH_LOW.addItem("welcome to high_and_low")
 label_HIGH_LOW.addItem("there is a number between 1 to 100")
 label_HIGH_LOW.addItem("guess it")
 line_edit = QLineEdit()
+line_edit.setAlignment(Qt.AlignCenter)
 keterangan = QLabel("your answer :")
-button_HIGH_LOW = QPushButton("start")
+line_edit.setFixedSize(300,150)
+button_HIGH_LOW = Button((100, 50), "START", 20)
 
-keterangan.setStyleSheet("font : 30px Arial")
+keterangan.setStyleSheet("font : 30px Arial;"
+                         "border : 5px solid black;"
+                         "background-color : white;"
+                         "color : black")
+line_edit.setStyleSheet("border : 5px solid black;"
+                        "border-radius : 20px;"
+                        "background-color : white;"
+                        "color : black;"
+                        "font : 50px Arial")
 """Home"""
 list_program = ["high and low", "roulette"]
 home_label = QLabel("WELCOME")
@@ -44,20 +79,28 @@ for i in range(len(list_program)):
     abcd.append(QRadioButton())
     abcd[i].setText(list_program[i])
 
-button_start = QPushButton("start")
-button_start.setStyleSheet("font : 50px Arial")
-button_start.setFixedHeight(100)
-button_start.setFixedWidth(200)
+button_start = Button((200, 100), "start")
+    
 
-abcd[0].setStyleSheet("font : 30px Arial")
+abcd[0].setStyleSheet("font : 30px Arial;"
+                      """QRadioButton::indicator {
+                      image: url (projek_app/asset/unchecked_box.png)
+                      }""")
 abcd[1].setStyleSheet("font : 30px Arial")
 
 
 """     qpixmap      """
 img_home = QPixmap("projek_app/asset/home_14782571.png")
 img_home = img_home.scaled(90, 90)
+home_label.setFixedSize(150,150)
 home_label.setPixmap(img_home)
+home_label.setStyleSheet("border : 2px solid cyan;"
+                        "border-radius : 20px;"
+                        "background-color : white")
+home_label.setAlignment(Qt.AlignCenter)
 """layout"""
+"""fishing"""
+fishing_layout = QVBoxLayout()
 """SHOOTING"""
 shooting_layout = QVBoxLayout()  
 second_shooting_layout = QHBoxLayout()
@@ -71,7 +114,7 @@ main_layout = QVBoxLayout()
 main_layout.addWidget(label_HIGH_LOW, alignment=Qt.AlignTop)
 main_layout.addWidget(keterangan, alignment=Qt.AlignCenter)
 main_layout.addWidget(line_edit, alignment=Qt.AlignCenter)
-main_layout.addWidget(button_HIGH_LOW)
+main_layout.addWidget(button_HIGH_LOW, alignment=Qt.AlignCenter )
 """HOME"""
 home_layout = QVBoxLayout()
 home_layout.addWidget(home_label, alignment=Qt.AlignCenter)
@@ -146,7 +189,6 @@ class shoot():
             self.reloading_gun()
         self.player_shoot()
         self.opponent_shoot()
-
 """high_low"""
 class high_and_low():
     def __init__(self):
@@ -162,9 +204,22 @@ class high_and_low():
         else :
             label_HIGH_LOW.addItem("YOU GUESS IT RIGHT")
             label_HIGH_LOW.addItem(f"YOUR ATTEMPT {self.counter}")
-        return "skibidi"
     
-    '''start'''
+"""iwak"""
+class iwak():
+    def __init__(self) -> None:
+        with open("iwak.json", 'r') as data :
+            self.data = json.load(data)
+    def gacha_time(self) :
+        gacha = random()
+        if gacha >= 0.95:
+            self.kepilih = random.choice(self.data["epic"])
+        elif gacha >= 0.9:
+            self.kepilih = random.choice(self.data["rare"])
+        else :
+            self.kepilih = random.choice(self.data["common"])
+
+'''start'''
 def check():
     if abcd[0].isChecked() or abcd[1].isChecked() :
         if abcd[0].isChecked() :
@@ -185,10 +240,12 @@ alamak = button_HIGH_LOW.clicked.connect(runner.game)
 button_shooting[0].clicked.connect(runner2.awalan_round)
 
 """window"""
-start_window = MainWindow("HOME", home_layout)
-home_window = MainWindow("Home", main_layout)
-shooting_window = MainWindow("shooting", shooting_layout)
+fishing_window = MainWindow("fish", fishing_layout, (700,500))
+start_window = MainWindow("start", home_layout, (450, 500))
+home_window = MainWindow("Home", main_layout, (450, 500))
+shooting_window = MainWindow("shooting", shooting_layout, (450, 500))
 
 """EXEC"""
+fishing_window.show()
 start_window.show()
 app.exec_()
